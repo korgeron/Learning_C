f2d1c003571073ea83d80f070a0b7e0587c1a549
korgeron <orgeron.kevin@gmail.com>
2022-06-23 21:40:28 -0500

    Add project files.

---
+++ learn_C (console.app)/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn_C__console.app_
+{
+   public class Person
+    {
+        private string name;
+
+
+        private void SayHello(string name)
+        {
+            Console.WriteLine("Hi there! My name is " + name);
+        }
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+
+
+       
+        public void getName()
+        {
+            Console.WriteLine(this.name);
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ learn_C (console.app)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn_C__console.app_
+{
+    class Program
+    {
+
+       
+
+
+        static void Main(string[] args)
+        {
+            string[] names = new string[7];
+            names[0] = "Karl";
+            names[1] = "Stan";
+            names[2] = "Vex";
+            names[3] = "Justin";
+            names[4] = "Sam";
+            names[5] = "Hector";
+            names[6] = "Luther";
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("```````````````````````````````````````````````````````````````````````````````````````````````````");
+
+            Console.WriteLine(names.Length);
+
+            Console.WriteLine("```````````````````````````````````````````````````````````````````````````````````````````````````");
+            
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("["+(i + 1)+"]" + " " + names[i]);
+
+            }
+
+            Console.WriteLine("```````````````````````````````````````````````````````````````````````````````````````````````````");
+
+            Person bill = new Person("Bill");
+            bill.getName();
+
+
+            Console.ReadLine();
+        }
+    }
+}
